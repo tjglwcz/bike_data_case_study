3897a5012969baa659a29bd9a55a4c369168fbfc
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2023-07-19 12:01:34 +0200

    Initial commit

---
+++ workbench.r
@@ -0,0 +1,46 @@
+# Initial setup - enabling tidyverse packages
+install.packages("tidyverse")
+library(tidyverse)
+
+
+# Importing all necessary data
+june2022 <- read_csv("csv/202206-divvy-tripdata.csv")
+july2022 <- read_csv("csv/202207-divvy-tripdata.csv")
+august2022 <- read_csv("csv/202208-divvy-tripdata.csv")
+september2022 <- read_csv("csv/202209-divvy-tripdata.csv")
+october2022 <- read_csv("csv/202210-divvy-tripdata.csv")
+november2022 <- read_csv("csv/202211-divvy-tripdata.csv")
+december2022 <- read_csv("csv/202212-divvy-tripdata.csv")
+january2023 <- read_csv("csv/202301-divvy-tripdata.csv")
+february2023 <- read_csv("csv/202302-divvy-tripdata.csv")
+march2023 <- read_csv("csv/202303-divvy-tripdata.csv")
+april2023 <- read_csv("csv/202304-divvy-tripdata.csv")
+may2023 <- read_csv("csv/202305-divvy-tripdata.csv")
+
+
+# Aggregating all data into single data frame
+df_list = list(june2022,july2022,august2022,september2022,october2022,november2022,december2022,january2023,february2023,march2023,april2023,may2023)
+all_trips <- df_list %>% reduce(left_join, by=colnames(june2022))
+
+# Removing irrelevant columns from the data frame
+colnames(all_trips)
+all_trips <- all_trips %>% select(-c(start_lat,start_lng,end_lat,end_lng))
+
+# Data frame inspection
+str(all_trips)
+head(all_trips)
+unique(all_trips$member_casual)
+
+# Adding additional columns with relevant information regarding the date and trip duration
+all_trips$ride_length <- c(all_trips$ended_at - all_trips$started_at)
+all_trips$ride_length <- as.numeric(all_trips$ride_length)
+
+all_trips$day_of_week <- c(wday(all_trips$started_at))
+
+# Initial data cleaning and verification
+all_trips <- all_trips %>% filter(ride_length > 0)
+all_trips <- all_trips %>% filter(!ride_length > 86400)
+all_trips <- unique(all_trips)
+all_trips_v2 <- all_trips %>% drop_na(start_station_name) %>% drop_na(end_station_name)
+
+write.csv(all_trips_v2, "C:\\Users\\Tomek\\Documents\\data_analysis\\bike_data\\data.csv")
